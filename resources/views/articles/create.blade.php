ae5b1d742f4b9edc28e0ec45c3b672bab2adc623
Ema998 <emanuele.gornati3@gmail.com>
2025-06-30 16:42:58 +0200

    commit

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,7 +2,7 @@
     <x-header>
         <h1 class="text-center">Create Article</h1>
     </x-header>
-    <livewire::create-form-article>
+    <livewire:create-form-article>
         
-    </livewire::create-form-article>
+    </livewire:create-form-article>
 </x-layout>
